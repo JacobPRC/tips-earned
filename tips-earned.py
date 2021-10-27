1b9bcb5bd66ef7d320ecfd28ad5a87fd03d23d89
jacob <jacobcunningham96@yahoo.com>
2021-10-26 17:10:55 -0700

    changed to read data from a csv and output as another csv

--- tips-earned.py
+++ tips-earned.py
@@ -1,19 +1,7 @@
 import numpy as np
 import csv
+import pandas as pd
 
-
-class Worker:
-    def __init__(self, name, hours_worked):
-        self.name = name
-        self.hours_worked = hours_worked
-
-
-jacob = Worker("Jacob", [5, 5, 5, 5, 5, 4.5, 5])
-tessa = Worker("Tessa", [8, 8, 8, 8, 8, 1, 1])
-derick = Worker("Derick", [0, 0, 1, 2.5, .25, 8, 8])
-morgan = Worker("Morgan", [0, 0, 4, 5.5, 2, 6, 9])
-
-tips = [1000, 500, 345.66, 333.10, 345.99, 999.10, 810.75]
 
 # Next steps:
 # Less for loops?
@@ -38,30 +26,77 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def tips_earned(workers, tips):
-    total_hours = np.repeat([0], len(tips))
-    names = []
+def tips_earned():
+    data = pd.read_csv(
+        r"C:\Users\jacob\Downloads\tips-earned - Sheet1.csv")
+
+    total_hours = np.repeat([0], len(data.Date))
+    workers = []
     earned_tips = []
 
-    for worker in workers:
-        hours = worker.hours_worked
-        name = worker.name
-        names.append(name)
+    for item in data.columns[2:]:
+        hours = data[item]
+        workers.append(item)
         for idx, hour in enumerate(hours):
             daily_hours = hour + total_hours[idx]
             total_hours[idx] = daily_hours
 
+    tips = data.Tips
     for index, tip in enumerate(tips):
-        for worker in workers:
-            percent_worked = (worker.hours_worked[index] / total_hours[index])
+        for item in data.columns[2:]:
+            hour = data[item][index]
+            percent_worked = (hour / total_hours[index])
             worker_tips = round(tip * percent_worked, 2)
             earned_tips.append(worker_tips)
 
-    rows = list(chunks(earned_tips, len(workers)))
+    data = list(chunks(earned_tips, len(data.columns) - 2))
+    print(data)
 
     print("We are printing a csv file to show how much tips your employees earned per hour. Please open using excel or google spreadsheets")
 
-    output_data(names, rows)
+    output_data(workers, data)
 
 
-tips_earned([jacob, tessa, derick, morgan], tips)
+tips_earned()
+
+
+# def tips_earned(workers, tips):
+#     total_hours = np.repeat([0], len(tips))
+#     names = []
+#     earned_tips = []
+
+#     for worker in workers:
+#         hours = worker.hours_worked
+# #         name = worker.name
+# #         names.append(name)
+#         for idx, hour in enumerate(hours):
+#             print(hour, worker.name)
+# #             daily_hours = hour + total_hours[idx]
+# #             total_hours[idx] = daily_hours
+
+# #     for index, tip in enumerate(tips):
+# #         for worker in workers:
+# #             percent_worked = (worker.hours_worked[index] / total_hours[index])
+# #             worker_tips = round(tip * percent_worked, 2)
+# #             earned_tips.append(worker_tips)
+
+# #     rows = list(chunks(earned_tips, len(workers)))
+
+# #     print("We are printing a csv file to show how much tips your employees earned per hour. Please open using excel or google spreadsheets")
+
+# #     output_data(names, rows)
+
+
+# tips_earned([jacob, tessa, derick, morgan], tips)
+
+# def output_data(data, columns):
+#     employee_tips = pd.DataFrame(data, columns=columns)
+#     employee_tips.to_csv("employee-tips-earned.csv")
+
+
+# # Shout out to Ned Batchelder on stack overflow for this consise code
+
+
+# def chunks(lst, n):
+#     for i in range(0, len(lst), n):
+#         yield lst[i:i + n]
